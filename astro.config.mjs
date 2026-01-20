4227b415e7ae829b4eab6de376ddc4789abd2041
Kazunori Hisada <hisadakn@noar.co.jp>
2026-01-20 12:21:30 +0900

    Remove unused repo variables from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-// Project Pages: https://<owner>.github.io/<repo>/ or User/Org Pages: https://<owner>.github.io/
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'recruit-lp';
-const repoOwner =
-  process.env.GITHUB_REPOSITORY_OWNER ??
-  process.env.GITHUB_REPOSITORY?.split('/')[0];
 const siteUrl = process.env.PUBLIC_SITE_URL ?? 'https://mcr.noar.biz';
 // https://astro.build/config
 export default defineConfig({
